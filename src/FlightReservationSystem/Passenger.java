fa9160ab61b67ee0d98f464d3f082fff4f256f11
Satyamrai09 <95238430+Satyamrai09@users.noreply.github.com>
2021-12-09 02:16:00 +0530

    chekpoint 3

--- src/FlightReservationSystem/Passenger.java
+++ src/FlightReservationSystem/Passenger.java
@@ -1,64 +1,76 @@
 package FlightReservationSystem;
 
 public class Passenger {
-   private int id;
-   public int getId(){
-       return id;
-   }
+    private int id;
+
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    Address address=new Address();
-Contact contact=new Contact();
-private static int idCounter=0;
+    Address address = new Passenger.Address();
+    Contact contact = new Passenger.Contact();
+    private static int idCounter = 0;
 
-Passenger(int id){
-    this.id=id;
-    idCounter++;
-}
+    Passenger(int id) {
+        id = idCounter++;
+    }
+    Passenger(){
 
-}
-class Address{
-   private String addressStreet;
-   public String getAddressStreet(){
-       return addressStreet;
-   }
-
-    public void setAddressStreet(String addressStreet) {
-        this.addressStreet = addressStreet;
+    }
+    public int  getPassengerMethod(){
+        return idCounter;
     }
 
-   private String addressCity;
-   public String getAddressCity(){
-       return addressCity;
-   }
 
-    public void setAddressCity(String addressCity) {
-        this.addressCity = addressCity;
+    private static class Address {
+        private String addressStreet;
+
+        public String getAddressStreet() {
+            return addressStreet;
+        }
+
+        public void setAddressStreet(String addressStreet) {
+            this.addressStreet = addressStreet;
+        }
+
+        private String addressCity;
+
+        public String getAddressCity() {
+            return addressCity;
+        }
+
+        public void setAddressCity(String addressCity) {
+            this.addressCity = addressCity;
+        }
+
+        String addressState;
     }
 
-    String addressState;
-}
-class Contact{
-   private String name;
-   public String getName(){
-       return name;
-   }
+    private static class Contact {
+        private String name;
 
-    public void setName(String name) {
-        this.name = name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String phoneNumber;
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        String Email;
     }
-
-    private String phoneNumber;
-   public String getPhoneNumber(){
-       return phoneNumber;
-   }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    String Email;
 }
