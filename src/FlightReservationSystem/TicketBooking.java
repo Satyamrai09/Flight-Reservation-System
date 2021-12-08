e8f31c53ecc15c9edde83fd138498c2958272878
Satyamrai09 <95238430+Satyamrai09@users.noreply.github.com>
2021-12-09 02:18:03 +0530

    checkpoint 3

--- src/FlightReservationSystem/TicketBooking.java
+++ src/FlightReservationSystem/TicketBooking.java
@@ -1,18 +1,42 @@
 package FlightReservationSystem;
 
 public  class TicketBooking {
-    String PNR;
-    String departureTime;
-    String destination;
-    String flightBooked;
-    String dateOfDeparture;
-    String dateOfArrival;
-    String timeOfArrival;
-    String timeOfDeparture;
-    int seatNumber;
-    float ticketPrice;
+   private  String PNR;
+   public String getPNR(){
+       return PNR;
+   }
+
+    public void setPNR(String PNR) {
+        this.PNR = PNR;
+    }
+
+    private  String departureTime;
+    private String destination;
+    private String flightBooked;
+    private String dateOfDeparture;
+    private String dateOfArrival;
+    private String timeOfArrival;
+    private String timeOfDeparture;
+   private  int seatNumber;
+   public int getSeatNumber(){
+       return seatNumber;
+   }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    private float ticketPrice;
+   public float getTicketPrice(){
+       return ticketPrice;
+   }
+   public void setTicketPrice(float ticketPrice){
+       this.ticketPrice=ticketPrice;
+   }
     boolean statusOfTicket;
    private String durationOfJourney;
+Flight flight;
+Passenger passenger;
 
     public boolean CancelTicket(){
        statusOfTicket=false;
@@ -29,5 +53,8 @@ public  class TicketBooking {
     TicketBooking (String durationOfJourney){
 
         this.durationOfJourney=durationOfJourney;
+    }
+    TicketBooking(){
+
     }
 }
